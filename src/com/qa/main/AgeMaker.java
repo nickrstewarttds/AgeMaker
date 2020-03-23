f4cd80683883080e2abc9a8ef05798d15c6261e0
Nick Johnson <nickrstewarttds@gmail.com>
2020-03-23 17:32:57 +0000

    init commit

---
+++ src/com/qa/main/AgeMaker.java
@@ -0,0 +1,16 @@
+package com.qa.main;
+
+public class AgeMaker {
+
+	public static int age = 25;
+	public int realAge = 100000000;
+
+	public static String writeSomething() {
+		return "Hello my friends, I am " + age + " years old :O";
+	}
+
+	public String writeSomethingElse() {
+		return "My real age is " + realAge;
+	}
+
+}
